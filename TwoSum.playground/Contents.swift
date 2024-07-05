65e8f0a8e533c2a5171c1cae2fa5b1d3a9cdbfa7
Puvi <puviiosdev@gmail.com>
2024-07-05 16:02:00 +0530

    two sum used forloop

--- TwoSum.playground/Contents.swift
+++ TwoSum.playground/Contents.swift
@@ -1,5 +1,7 @@
 import UIKit
 
+// https://leetcode.com/problems/two-sum/
+
 /*
  Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
